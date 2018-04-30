d743cbd70fc55fcd6a1b189d81c57c23760f64b7
marmanillo <erick28_06@hotmail.com>
2018-04-30 12:14:56 +0200

    Sample

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'rails', '~> 5.2.0'
 gem 'bootstrap-sass', '3.3.7'
 #gema für test:integration
 gem 'rails-controller-testing'
+#gema für password
+gem 'bcrypt', '3.1.11'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_27_115937) do
+ActiveRecord::Schema.define(version: 2018_04_30_091235) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "string"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

4e66384c2568d4f7556356e6e9f83422a77ac3bd
marmanillo <erick28_06@hotmail.com>
2018-05-07 14:53:11 +0200

    Für heroku: Postgres statt sqlite3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,41 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    #debugger
   end
+
   def new
-    #debugger
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user #redirect_to user_url(@user)
+      redirect_to @user
     else
-      flash.now[:alert] = "Complete the Fields!"
+      #flash.now[:alert] = "Complete the Fields!"
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
 end
